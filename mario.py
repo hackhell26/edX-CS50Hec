1aef313078f211c1262a928845733b1117998f77
hackhell26 <95175755+hackhell26@users.noreply.github.com>
2022-05-21 09:09:40 +0000

    while loop

--- mario.py
+++ mario.py
@@ -1,6 +1,10 @@
 from cs50 import get_int
 
-n = get_int("Height: ")
+# validation for no positive numbers
+while True: # do while loop doesn't exist in python
+    n = get_int("Height: ") # do whatever you have to do
+    if n > 0: # and if it is that what i want, break the program
+        break
 
 for i in range(n):
     print("#")
