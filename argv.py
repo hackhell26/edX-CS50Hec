d68f8fc288f53552abbd05cada0a0b25d2a6de80
hackhell26 <95175755+hackhell26@users.noreply.github.com>
2022-05-23 13:02:52 +0000

    slicing the argv list

--- argv.py
+++ argv.py
@@ -1,5 +1,5 @@
 from sys import argv #argv it is a list
 
-for arg in argv: # for loop to print out all the CLI arguments that i type in
+for arg in argv[1:]: # slicing the argv list, starting at position 1 going all of the way to the ende
     if arg != "argv.py":
         print(arg)
