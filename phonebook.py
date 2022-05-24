7119349fbd2277cb9749c9f2909ebfd0866c0962
hackhell26 <95175755+hackhell26@users.noreply.github.com>
2022-05-24 11:48:24 +0000

    first commit

--- phonebook.py
+++ phonebook.py
@@ -1,15 +1,2 @@
-from cs50 import get_string
+import csv
 
-# the idea in this is associate keys with values
-people = {
-    "Hector" : "666196831",
-    "Dayana" : "04141565657"
-}
-
-name = get_string("Name: " )
-if name in people:
-    print(f"Number: {people[name]}")
-    exit(0)
-
-print("Not found")
-exit(1)
